6024f5cac1464fcbd87485e7543811798883460f
Nikunj Gupta <nikunj.gupta447@gmail.com>
2018-11-25 16:17:53 +0530

    rubic cube mouse rotate

--- script.js
+++ script.js
@@ -18,4 +18,57 @@ for (var i = 0; i < faces.length; i++) {
   faces[i].style.width = side + "px";
   faces[i].style.height = side + "px";
 }
-console.log(side);
+
+var horizontalRotationCount = 0;
+var verticalRotationCount = 0;
+var x, y, newX, newY;
+var transformations = [];
+cube.onmousedown = e => {
+  x = e.screenX;
+  y = e.screenY;
+};
+cube.onmouseup = e => {
+  newX = e.screenX;
+  newY = e.screenY;
+
+  //horizontal
+  if (Math.abs(x - newX) > Math.abs(y - newY)) {
+    // right
+    if (newX > x) {
+      horizontalRotationCount += 1;
+      transformations.push(`rotateY(90deg)`);
+    }
+    // left
+    else {
+      horizontalRotationCount -= 1;
+      transformations.push(`rotateY(-90deg)`);
+    }
+    // cube.style.transform = transformations.reverse().join(" ");
+
+    cube.style.transform = `rotateY(${horizontalRotationCount *
+      90}deg) rotateX(${verticalRotationCount * 90}deg)`;
+  }
+  //vertical
+  else {
+    // up
+    if (newY < y) {
+      verticalRotationCount += 1;
+      transformations.push(`rotateX(90deg)`);
+    }
+    // down
+    else {
+      verticalRotationCount -= 1;
+      transformations.push(`rotateX(-90deg)`);
+    }
+    // cube.style.transform = transformations.reverse().join(" ");
+    cube.style.transform = `rotateX(${verticalRotationCount *
+      90}deg) rotateY(${horizontalRotationCount * 90}deg)`;
+  }
+  console.log(
+    horizontalRotationCount +
+      " " +
+      verticalRotationCount +
+      " " +
+      cube.style.transform
+  );
+};
