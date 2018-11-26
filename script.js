c09cf5c23276fcbf6423082c082cc6133ebf4f6a
Nikunj Gupta <nikunj.gupta447@gmail.com>
2018-11-26 10:30:04 +0530

    om mousedrag added to the cube

--- script.js
+++ script.js
@@ -2,31 +2,42 @@ var container = document.getElementById("container");
 var cube = document.getElementById("cube");
 var faces = document.getElementsByClassName("face");
 
-var side =
-  window.innerHeight < window.innerWidth
-    ? window.innerHeight / 2
-    : window.innerWidth / 2;
+//autoSiziing
+function autoSize() {
+  var side =
+    window.innerHeight < window.innerWidth
+      ? window.innerHeight / 2
+      : window.innerWidth / 2;
 
-container.style.width = side + "px";
-container.style.height = side + "px";
-container.style.margin = side / 2 + "px auto 0";
+  container.style.width = side + "px";
+  container.style.height = side + "px";
+  container.style.margin = side / 2 + "px auto 0";
 
-cube.style.width = side + "px";
-cube.style.height = side + "px";
+  cube.style.width = side + "px";
+  cube.style.height = side + "px";
 
-for (var i = 0; i < faces.length; i++) {
-  faces[i].style.width = side + "px";
-  faces[i].style.height = side + "px";
+  for (var i = 0; i < faces.length; i++) {
+    faces[i].style.width = side + "px";
+    faces[i].style.height = side + "px";
+  }
 }
 
-var horizontalRotationCount = 0;
-var verticalRotationCount = 0;
+function getTransformString(transformations) {
+  var transformString = "";
+  for (var i = transformations.length - 1; i >= 0; i--) {
+    transformString = transformString + " " + transformations[i];
+  }
+  return transformString;
+}
+
 var x, y, newX, newY;
 var transformations = [];
+
 cube.onmousedown = e => {
   x = e.screenX;
   y = e.screenY;
 };
+
 cube.onmouseup = e => {
   newX = e.screenX;
   newY = e.screenY;
@@ -35,40 +46,27 @@ cube.onmouseup = e => {
   if (Math.abs(x - newX) > Math.abs(y - newY)) {
     // right
     if (newX > x) {
-      horizontalRotationCount += 1;
       transformations.push(`rotateY(90deg)`);
     }
     // left
     else {
-      horizontalRotationCount -= 1;
       transformations.push(`rotateY(-90deg)`);
     }
-    // cube.style.transform = transformations.reverse().join(" ");
-
-    cube.style.transform = `rotateY(${horizontalRotationCount *
-      90}deg) rotateX(${verticalRotationCount * 90}deg)`;
   }
   //vertical
   else {
     // up
     if (newY < y) {
-      verticalRotationCount += 1;
       transformations.push(`rotateX(90deg)`);
     }
     // down
     else {
-      verticalRotationCount -= 1;
       transformations.push(`rotateX(-90deg)`);
     }
-    // cube.style.transform = transformations.reverse().join(" ");
-    cube.style.transform = `rotateX(${verticalRotationCount *
-      90}deg) rotateY(${horizontalRotationCount * 90}deg)`;
   }
-  console.log(
-    horizontalRotationCount +
-      " " +
-      verticalRotationCount +
-      " " +
-      cube.style.transform
-  );
+
+  cube.style.transform = getTransformString(transformations);
+  console.log(transformations);
 };
+
+autoSize();
